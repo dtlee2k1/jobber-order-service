865a41886521c4809177636d7fcc272bdf4e128c
Le Duc Thai <leducthai2001@gmail.com>
2024-03-19 21:11:26 +0700

    add order & notification schemas

---
+++ src/models/notification.schema.ts
@@ -0,0 +1,17 @@
+import { IOrderNotifcation } from '@dtlee2k1/jobber-shared';
+import { model, Model, Schema } from 'mongoose';
+
+const notificationSchema: Schema = new Schema({
+  userTo: { type: String, default: '', index: true },
+  senderUsername: { type: String, default: '' },
+  senderPicture: { type: String, default: '' },
+  receiverUsername: { type: String, default: '' },
+  receiverPicture: { type: String, default: '' },
+  isRead: { type: Boolean, default: false },
+  message: { type: String, default: '' },
+  orderId: { type: String, default: '' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const OrderNotificationModel: Model<IOrderNotifcation> = model<IOrderNotifcation>('OrderNotification', notificationSchema);
+export { OrderNotificationModel };
