78c9fe64be9f41749613114523399336c89cfc1b
Le Duc Thai <leducthai2001@gmail.com>
2024-03-19 21:41:15 +0700

    handle notification service methods

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ class Config {
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
   public GATEWAY_JWT_TOKEN: string | undefined;
+  public CLIENT_URL: string | undefined;
   public API_GATEWAY_URL: string | undefined;
   public ELASTIC_SEARCH_URL: string | undefined;
 
@@ -24,6 +25,7 @@ class Config {
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.API_GATEWAY_URL = process.env.API_GATEWAY_URL || '';
     this.ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL || '';
   }
---
+++ src/services/notification.service.ts
@@ -0,0 +1,40 @@
+import { IOrderDocument, IOrderNotifcation } from '@dtlee2k1/jobber-shared';
+import { OrderNotificationModel } from '@order/models/notification.schema';
+import { socketIOOrderObject } from '@order/server';
+
+export async function createNotification(data: IOrderNotifcation) {
+  const notification: IOrderNotifcation = await OrderNotificationModel.create(data);
+  return notification;
+}
+
+export async function getNotificationsById(userToId: string) {
+  const notifications: IOrderNotifcation[] = await OrderNotificationModel.aggregate([
+    {
+      $match: { userTo: userToId }
+    }
+  ]);
+  return notifications;
+}
+
+export async function markNotificationAsRead(notificationId: string) {
+  const notification: IOrderNotifcation = (await OrderNotificationModel.findOneAndUpdate(
+    { _id: notificationId },
+    { $set: { isRead: true } },
+    { new: true }
+  )) as IOrderNotifcation;
+  return notification;
+}
+
+export async function sendNotification(data: IOrderDocument, userToId: string, message: string) {
+  const notification: IOrderNotifcation = {
+    userTo: userToId,
+    senderUsername: data.sellerUsername,
+    senderPicture: data.sellerImage,
+    receiverUsername: data.buyerUsername,
+    receiverPicture: data.buyerImage,
+    message,
+    orderId: data.orderId
+  } as IOrderNotifcation;
+  const orderNotification: IOrderNotifcation = await createNotification(notification);
+  socketIOOrderObject.emit('order_notification', data, orderNotification);
+}
